670d0adfc97145f3e46c8311090482121ec73c1c
Katrin <katyaserova@yandex.ru>
2026-03-09 14:38:58 +0300

    Подготовка к деплою на Amvera: настройки, requirements и конфиг

--- japan_cook/settings.py
+++ japan_cook/settings.py
@@ -6,7 +6,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # === БЕЗОПАСНОСТЬ ===
 SECRET_KEY = config('SECRET_KEY')
-DEBUG = True
+DEBUG = False
 ALLOWED_HOSTS = []
 
 # === ПРИЛОЖЕНИЯ ===
@@ -23,6 +23,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware', 
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -81,8 +82,11 @@ USE_TZ = True
 
 # === СТАТИЧЕСКИЕ ФАЙЛЫ ===
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / 'static']
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = BASE_DIR / 'static'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = '/data/media'
 
 # === МЕДИА ФАЙЛЫ ===
 MEDIA_URL = '/media/'
